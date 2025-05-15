d7ae636f50f8b1990ef61c2eebb4953bcb6440f7
Weber Federico <sweber@frh.utn.edu.ar>
2025-05-15 16:33:35 -0300

    Agrego material clase Argumentos del Main

---
+++ C/Argumentos del Main/ejemplo06.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char *argv[]) {
+    if (argc < 2) {
+        fprintf(stdout, "Error: falta un número.\n");
+        return 1;
+    }
+
+    char *fin;
+    float valor = strtof(argv[1], &fin);
+
+    if (*fin != '\0') {
+        fprintf(stdout,"Error: '%s' no es un numero valido.\n", argv[1]);
+        return 1;
+    }
+
+    printf("[strtof] Valor convertido: %.2f\n", valor);
+    return 0;
+}
+
